337b021f390dd12fa3e8c72cdbd7ad56f4a4a723
Eric Berquist <erb74@pitt.edu>
2013-08-06 17:36:18 -0400

    I think we might be getting somewhere...

--- gold/build_armadillo-3.900.7-sequential.sh
+++ gold/build_armadillo-3.900.7-sequential.sh
@@ -4,6 +4,7 @@ version=3.900.7-sequential
 
 source /etc/profile.d/modules.sh
 module purge
+module load gcc/builtin/4.4.7
 module load intel/2011
 module load mkl/2011/icc-st
 module load armadillo/${version}
@@ -12,28 +13,10 @@ rm -rf $HOME/opt/build/armadillo-${version}-builddir >& /dev/null
 mkdir  $HOME/opt/build/armadillo-${version}-builddir
 cd     $HOME/opt/build/armadillo-${version}-builddir
 
-ABORT=no
-
-check_cmake()
-{
-    (cmake --version) </dev/null >/dev/null 2>&1 ||
-    {
-	echo "error: cmake (version 2.6 or later) must be present to configure and install Armadillo"
-	echo ""
-	echo "cmake might be available as a package for your system,"
-	echo "or can be downloaded from http://cmake.org"
-	ABORT=yes
-    }
-}
-
-check_cmake
-
-test "$ABORT" = yes && exit -1
-
 cmake \
     -DCMAKE_INSTALL_PREFIX=$HOME/opt/armadillo/${version} \
-    -DCMAKE_CXX_COMPILER=g++ \
-    -DCMAKE_LINKER=ld \
+    -DCMAKE_CXX_COMPILER=icpc \
+    -DCMAKE_LINKER=xild \
     ../armadillo-${version}
 
 make -j8
