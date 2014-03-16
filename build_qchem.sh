0e2c8d6896c6491464b015a3284edf0428b5b5fe
Eric Berquist <erb74@pitt.edu>
2014-03-16 14:26:28 -0400

    Module-independent script for Q-Chem. Run once, then just 'make' in build/ unless major changes.

---
+++ build_qchem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd $QC
+
+./configure intel release mkl openmp
+
+cd $QC/build
+
+sed -i -e 's/-O2/-O0/g' $QC/build/CMakeCache.txt
+sed -i -e 's/-O3/-O0/g' $QC/build/CMakeCache.txt
+
+make generated_headers
+make -j2
+
+mkdir $QC/exe 2> /dev/null
+ln -fs $QC/build/qcprog.exe         $QC/exe/qcprog.exe
+ln -fs $QC/build/diffParseAlone.exe $QC/util/cronutil/diffParseAlone.exe
+ln -fs $QC/build/mergeLines.exe     $QC/util/cronutil/mergeLines.exe
