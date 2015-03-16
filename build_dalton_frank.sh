b5eefb4e42d0625777d2cd36eb995338362f466c
Eric Berquist <erb74@pitt.edu>
2015-03-16 17:24:45 -0400

    Not sure what version this is for...

---
+++ build_dalton_frank.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge
+module load modules use.own
+module load intel/2013.0
+module load openmpi/1.6.4-intel13-i8
+module load mkl/2013.0/icc-st-openmpi-i8
+module load boost/1.51.0-intel13
+
+cd ./DALTON
+./configure
