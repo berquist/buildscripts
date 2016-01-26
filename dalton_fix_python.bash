75b701c0eaeb1d6f706a9c047b1d6d5df690b101
Eric Berquist <eric.berquist@gmail.com>
2016-01-26 17:16:02 -0500

    Fix all Python files in DALTON/DIRAC source trees.

---
+++ dalton_fix_python.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# dalton_fix_python.bash: Change all instances of `python` in the
+# DALTON source tree to `python2`, since the scripts aren't Python 3+
+# compatible.
+
+files=$(find . -type f)
+
+for file in ${files[@]};
+do
+    sed -i 's|python$|python2|g' "${file}"
+    sed -i 's|python2 |python2 |g' "${file}"
+done
