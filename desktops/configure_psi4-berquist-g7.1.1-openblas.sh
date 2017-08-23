ef3de339c6e85fab71fca0708eccc6387068c393
Eric Berquist <eric.berquist@gmail.com>
2017-08-22 20:20:15 -0400

    [desktops] remove old Psi4 and replace with personal branch

---
+++ desktops/configure_psi4-berquist-g7.1.1-openblas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# source me!
+
+ver=berquist-g7.1.1-openblas
+dir_source="${apps}"/psi4/src_berquist
+dir_build="${apps}"/build/psi4_${ver}
+
+module purge
+
+# rm -rf "${dir_build}"
+mkdir -p "${dir_build}"
+cd "${dir_build}"
+
+cmake \
+      -DCMAKE_INSTALL_PREFIX="${apps}"/psi4/${ver} \
+      -DPYTHON_EXECUTABLE=/usr/bin/python \
+      -DPYTHON_LIBRARY=/usr/lib/libpython3.so \
+      -DENABLE_dkh=ON \
+      "$dir_source"
+
+# make all -j5
+# make test
+# make install
