eb60ce53ea38ef1ed236678020e3f20f92bf9016
Eric Berquist <eric.berquist@gmail.com>
2018-05-10 16:12:26 -0400

    [desktops] be more explicit in turning off stuff for personal Psi4

--- desktops/configure_psi4-berquist-gnu-openblas.sh
+++ desktops/configure_psi4-berquist-gnu-openblas.sh
@@ -16,7 +16,16 @@ cmake \
       -DCMAKE_INSTALL_PREFIX="${apps}"/psi4/${ver} \
       -DPYTHON_EXECUTABLE=/usr/bin/python \
       -DPYTHON_LIBRARY=/usr/lib/libpython3.so \
+      -DENABLE_ambit=OFF \
+      -DENABLE_CheMPS2=OFF \
+      -DENABLE_libefp=OFF \
       -DENABLE_dkh=ON \
+      -DENABLE_erd=OFF \
+      -DENABLE_simint=OFF \
+      -DENABLE_gdma=OFF \
+      -DENABLE_PCMSolver=OFF \
+      -DENABLE_snsmp2=OFF \
+      -DENABLE_v2rdm_casscf=OFF \
       "$dir_source"
 
 # make all -j5
