112fdde1db89078ae3e9700c517ffcc6be355c80
Eric Berquist <eric.berquist@gmail.com>
2018-05-11 17:06:56 -0400

    [desktops] clean up DALTON

--- desktops/configure_dalton_git-gnu-openblas-omp.sh
+++ desktops/configure_dalton_git-gnu-openblas-omp.sh
@@ -6,23 +6,33 @@ ver=git-gnu-openblas-omp
 dir_build="${apps}"/build/dalton_"${ver}"
 dir_source="${HOME}"/repositories/dalton
 
-# source /etc/profile.d/modules.sh
+mkdir -p "${dir_build}"
+cd "${dir_build}"
 
-# module purge
-# module load dalton/${ver}
+# python2 ./setup \
+#         --prefix="${apps}/dalton/${ver}" \
+#         --fc=gfortran-6 \
+#         --cc=gcc-6 \
+#         --cxx=g++-6 \
+#         --gpu \
+#         --cublas \
+#         --omp \
+#         "${dir_build}"
 
-rm -rf "${dir_build}"
-mkdir "${dir_build}"
-cd "${dir_source}"
+cmake \
+    -DCMAKE_INSTALL_PREFIX="${apps}/dalton/${ver}" \
+    -DCMAKE_Fortran_COMPILER=gfortran-6 \
+    -DCMAKE_C_COMPILER=gcc-6 \
+    -DCMAKE_CXX_COMPILER=g++-6 \
+    -DENABLE_GPU=ON \
+    -DENABLE_CUDA=ON \
+    -DENABLE_CUBLAS=ON \
+    -DENABLE_OMP=ON \
+    -DENABLE_AUTO_BLAS=ON \
+    -DENABLE_AUTO_LAPACK=ON \
+    "${dir_source}"
 
-# --prefix="${DALTON_ROOT}" \
 
-./setup \
-    --prefix="${apps}/dalton/${ver}" \
-    --omp \
-    "${dir_build}"
-
-# cd "${dir_build}"
 # make all -j4
 # ctest -j4
 # make install
