770ac5f27a430de26f6f84002c68fb2537ed7553
Eric Berquist <eric.berquist@gmail.com>
2018-12-21 23:45:34 -0500

    [osmium] update Open Babel

--- osmium/configure_openbabel-git.sh
+++ osmium/configure_openbabel-git.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # source me!
 
 ver=git
-dir_source=$HOME/repositories/openbabel
-dir_build="${apps}"/build/openbabel_${ver}
+dir_source="${HOME}"/repositories/openbabel
+dir_build="${apps}"/build/openbabel_"${ver}"
 
 mkdir -p "${dir_build}"
 cd "${dir_build}"
 
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${apps}"/openbabel/${ver} \
+    -DCMAKE_INSTALL_PREFIX="${apps}"/openbabel/"${ver}" \
     -DRUN_SWIG=ON \
     -DBUILD_DOCS=ON \
     -DENABLE_OPENMP=ON \
     -DPHP_BINDINGS=OFF \
     -DPYTHON_BINDINGS=ON \
-    -DRUBY_BINDINGS=ON \
-    -DPERL_BINDINGS=ON \
-    -DJAVA_BINDINGS=ON \
+    -DRUBY_BINDINGS=OFF \
+    -DPERL_BINDINGS=OFF \
+    -DJAVA_BINDINGS=OFF \
     -DR_BINDINGS=OFF \
     -DCSHARP_BINDINGS=OFF \
     "${dir_source}"
